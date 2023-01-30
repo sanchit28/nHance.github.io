b0614ec20d1ea055778f001ddda23d2add84fa24
Sanchit Sharma <sanchit.sharma2899@gmail.com>
2023-01-31 00:30:03 +0530

    Update Create.js

--- Create.js
+++ Create.js
@@ -2,7 +2,7 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 //const phone = document.getElementById('phone');
-/*------------------------------------------------------------------------ */
+/*------------------------adding event listner for click------------------------------------------------ */
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
 
@@ -30,19 +30,23 @@ const SuccessMsg = (usernameVal) => {
     }
 }
 
-/*-----------------------------email verification function------------------------------------------- */
+/*-----------------------------for email verification ------------------------------------------- */
 const isEmail = (emailVal) =>{
     var atSymbol = emailVal.indexOf('@');
     if(atSymbol < 1) return false;
+
     var dot = emailVal.lastIndexOf('.');
     if(dot <= atSymbol + 2) return false;
     if(dot === emailVal.length -1) return false;
     return true;
 }
+/* extra function for mail verification */
+function isValidEmail(email){
+    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+ }
 /*-------------------------------Account Creation validations----------------------------------------- */
 function Validate(){
     const usernameVal = username.value.trim();
-    
     const emailVal = email.value.trim();
     const passwordVal = password.value.trim();
    // const phoneVal = cpassword.value.trim();
@@ -52,14 +56,11 @@ function Validate(){
         setErrorMsg(username, 'name cannot be blank');
     }
     else if(usernameVal.length <=5){
-        setErrorMsg(username, 'min 5 chars');
+        setErrorMsg(username, 'min. 5 chars');
     }
     else{
         setSuccessMsg(username);
     }
-
-    
-
     //email
     if(emailVal === ""){
         setErrorMsg(email, 'email cannot be blank');
@@ -76,7 +77,7 @@ function Validate(){
         setErrorMsg(password, 'password cannot be blank');
     }
     else if(passwordVal.length <= 7){
-        setErrorMsg(password, 'min 8 char');
+        setErrorMsg(password, 'min. 8 char');
     }
     else{
         setSuccessMsg(password);
@@ -97,14 +98,14 @@ function Validate(){
 
 
 }
-/*------------------------------------------------------------------------ */
+/*-------------------------------for error----------------------------------------- */
 function setErrorMsg(input, errormsgs){
-    const formControl = input.parentElement;
+    const formControl = input.parentElement; // will find parent element inour case-form-control
     const small = formControl.querySelector('small');
     formControl.className = "form-control error";
     small.innerText = errormsgs;
 }
-
+/*-------------------------------for successful match----------------------------------------- */
 function setSuccessMsg(input){
     const formControl = input.parentElement;
     formControl.className = "form-control success";
