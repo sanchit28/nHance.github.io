82e6cbefa16b850737677316537ec773ae4b9ff2
Sanchit Sharma <sanchit.sharma2899@gmail.com>
2023-01-30 14:29:19 +0530

    Js validations
    major js validation for login

--- Create.js
+++ Create.js
@@ -3,5 +3,24 @@
 
 
 function Hello() {
-    alert("Hey, Welcome to nHance");
+    var username = document.getElementById("name").value
+    alert("Hey, Welcome to nHance,"+ username);
  }
+/*
+ function validate(){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    for(i = 0; i < database.length; i++){
+        if(username == database[i].username && password == database[i].password){
+            alert("Successfull Login, Hola Visionary");
+            //console.log("Successfull Login, Hola Visionary");
+            return
+        }//else{
+            //alert("Login Failed!! Try with correct username & password");
+            //console.log("Login Failed!! Try Again..");
+        //}
+        
+    }alert("Login Failed!! Try with correct username & password");
+    //console.log("Login Failed!! Try Again..");
+}
+*/
--- login.js
+++ login.js
@@ -1,13 +1,64 @@
+/*
+var obj=[
+    {
+        username : "root",
+        password : "root1234"
+    },
+    {
+        username : "test",
+        password : "test1234"
+    }
+]
+
+function getInfo(){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    console.log("Your username:"+ username +"/password:"+ password);
+}
+
+*/
+var database = [
+    {
+        username : "root",
+        password : "root1234"
+    },
+    {
+        username : "test",
+        password : "test1234"
+    },
+    {
+        username : "sanchit",
+        password : "sanchit1234"
+    }
+]
 function validate(){
-    var Username = document.getElementById("Username").value;
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    for(i = 0; i < database.length; i++){
+        if(username == database[i].username && password == database[i].password){
+            alert("Successfull Login, Hola Visionary");
+            //console.log("Successfull Login, Hola Visionary");
+            return
+        }//else{
+            //alert("Login Failed!! Try with correct username & password");
+            //console.log("Login Failed!! Try Again..");
+        //}
+        
+    }alert("Login Failed!! Try with correct username & password");
+    //console.log("Login Failed!! Try Again..");
+}
+/*
+function validate(){
+    var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     {
-        if(Username =="root" && password == "root@123"){
+        if(username =="root" && password == "root1234"){
             alert("Successfull Login, Hola Visionary");
-            return false;
+          return false;
         }
         else{
             alert("Login Failed!! Try Again..");
         }
     }
 }
+*/
