c8ccb1e9dbcff68bbee29bb5e9ac35767838ae10
Sanchit Sharma <sanchit.sharma2899@gmail.com>
2023-01-31 00:12:30 +0530

    create account validations
    Major changes for create validation

--- Create.js
+++ Create.js
@@ -1,26 +1,111 @@
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+//const phone = document.getElementById('phone');
+/*------------------------------------------------------------------------ */
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
 
-
-
-
-function Hello() {
-    var username = document.getElementById("name").value
-    alert("Hey, Welcome to nHance,"+ username);
- }
-/*
- function validate(){
-    var username = document.getElementById("username").value
-    var password = document.getElementById("password").value
-    for(i = 0; i < database.length; i++){
-        if(username == database[i].username && password == database[i].password){
-            alert("Successfull Login, Hola Visionary");
-            //console.log("Successfull Login, Hola Visionary");
-            return
-        }//else{
-            //alert("Login Failed!! Try with correct username & password");
-            //console.log("Login Failed!! Try Again..");
-        //}
-        
-    }alert("Login Failed!! Try with correct username & password");
-    //console.log("Login Failed!! Try Again..");
+    Validate();
+})
+/*------------------------------------------------------------------------ */
+const sendData = (usernameVal, sRate, Count) => {
+    if(sRate === Count){
+        swal("Hello " + usernameVal , "You are Registered", "success");
+    }
 }
+/*------------------------------------------------------------------------ */
+const SuccessMsg = (usernameVal) => {
+    let formContr = document.getElementsByClassName('form-control');
+    var Count = formContr.length - 1;
+    for(var i = 0; i < formContr.length; i++){
+        if(formContr[i].className === "form-control success"){
+            var sRate = 0 + i;
+            console.log(sRate);
+            sendData(usernameVal, sRate, Count);
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+/*-----------------------------email verification function------------------------------------------- */
+const isEmail = (emailVal) =>{
+    var atSymbol = emailVal.indexOf('@');
+    if(atSymbol < 1) return false;
+    var dot = emailVal.lastIndexOf('.');
+    if(dot <= atSymbol + 2) return false;
+    if(dot === emailVal.length -1) return false;
+    return true;
+}
+/*-------------------------------Account Creation validations----------------------------------------- */
+function Validate(){
+    const usernameVal = username.value.trim();
+    
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+   // const phoneVal = cpassword.value.trim();
+
+    //username
+    if(usernameVal === ""){
+        setErrorMsg(username, 'name cannot be blank');
+    }
+    else if(usernameVal.length <=5){
+        setErrorMsg(username, 'min 5 chars');
+    }
+    else{
+        setSuccessMsg(username);
+    }
+
+    
+
+    //email
+    if(emailVal === ""){
+        setErrorMsg(email, 'email cannot be blank');
+    }
+    else if(!isEmail(emailVal)){
+        setErrorMsg(email, 'email is not valid');
+    }
+    else{
+        setSuccessMsg(email);
+    }
+
+    //password
+    if(passwordVal === ""){
+        setErrorMsg(password, 'password cannot be blank');
+    }
+    else if(passwordVal.length <= 7){
+        setErrorMsg(password, 'min 8 char');
+    }
+    else{
+        setSuccessMsg(password);
+    }
+    /*
+    //confirm contact
+    if(phoneVal === ""){
+        setErrorMsg(phone, 'phome number cannot be blank');
+    }
+    else if(phoneVal < 10){
+        setErrorMsg(phone, 'Enter 10 digits number');
+    }
+    else{
+        setSuccessMsg(phone);
+    }
 */
+    SuccessMsg(usernameVal);
+
+
+}
+/*------------------------------------------------------------------------ */
+function setErrorMsg(input, errormsgs){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = "form-control error";
+    small.innerText = errormsgs;
+}
+
+function setSuccessMsg(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
