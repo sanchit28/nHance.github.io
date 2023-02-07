a13065f0f2e68a00d8bd253848eda4c8ec74ff47
Sanchit <sanchit.sharma2899@gmail.com>
2023-02-07 18:19:34 +0530

    Updates for validations
    Validations update and pages interface

--- Create.js
+++ Create.js
@@ -1,7 +1,12 @@
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-//const phone = document.getElementById('phone');
+const phone = document.getElementById('phone');
+/*additional variables */
+var regEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;  
+var regPhone = /^\d{10}$/;                                        
+var regName = /^[a-zA-Z\-]+$/; 
+var regPass_1 = /^[A-Za-z]\w{7,14}$/;
 /*------------------------adding event listner for click------------------------------------------------ */
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
@@ -11,69 +16,86 @@ form.addEventListener('submit', (event) =>{
 
 
 /*-----------------------------for email verification ------------------------------------------- */
-const isEmail = (emailVal) =>{
-    var atSymbol = emailVal.indexOf('@');
-    if(atSymbol < 1) return false;
+// const isEmail = (emailVal) =>{
+//     var atSymbol = emailVal.indexOf('@');
+//     if(atSymbol < 1) return false;
 
-    var dot = emailVal.lastIndexOf('.');
-    if(dot <= atSymbol + 2) return false;
-    if(dot === emailVal.length -1) return false;
-    return true;
-}
-/* extra function for mail verification with regex*/
-function isValidEmail(email){
-    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
- }
+//     var dot = emailVal.lastIndexOf('.');
+//     if(dot <= atSymbol + 2) return false;
+//     if(dot === emailVal.length -1) return false;
+//     return true;
+// }
+
+// /* extra function for mail verification with regex // ======Not used for validations==========*/
+// function isValidEmail(email){
+//     return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+//  }
 /*-------------------------------Account Creation validations----------------------------------------- */
 function Validate(){
     const usernameVal = username.value.trim();
     const emailVal = email.value.trim();
     const passwordVal = password.value.trim();
-   // const phoneVal = cpassword.value.trim();
+    const phoneVal = phone.value.trim();
 
-    //username
+    //------------------------username
     if(usernameVal === ""){
         setErrorMsg(username, 'name cannot be blank');
     }
     else if(usernameVal.length <=5){
         setErrorMsg(username, 'min. 5 chars');
     }
+    else if( !regName.test(usernameVal) ){
+        setErrorMsg(username, 'Digits cannot be used');
+        username.focus;
+    }
     else{
         setSuccessMsg(username);
     }
-    //email
+    //-----------------------email
     if(emailVal === ""){
         setErrorMsg(email, 'email cannot be blank');
     }
-    else if(!isEmail(emailVal)){
+    // else if(!isEmail(emailVal)){
+    //     setErrorMsg(email, 'email is not valid');
+    // }
+    else if( !regEmail.test(emailVal) ){
         setErrorMsg(email, 'email is not valid');
+        
     }
     else{
         setSuccessMsg(email);
     }
 
-    //password
+    //--------------------password
     if(passwordVal === ""){
         setErrorMsg(password, 'password cannot be blank');
     }
-    else if(passwordVal.length <= 7){
-        setErrorMsg(password, 'min. 8 char');
+    // else if(passwordVal.length <= 7){
+    //     setErrorMsg(password, 'min. 8 char');
+    // }
+    else if( !regPass_1.test(passwordVal) ){
+        setErrorMsg(password, 'min. 8 char;Should contain atleat one digit,1 uppercase & 1 lowercase');
+        
     }
     else{
         setSuccessMsg(password);
     }
-    /*
-    //confirm contact
+    
+    //-----------------------confirm contact
     if(phoneVal === ""){
         setErrorMsg(phone, 'phome number cannot be blank');
     }
-    else if(phoneVal < 10){
-        setErrorMsg(phone, 'Enter 10 digits number');
+    else if( !regPhone.test(phoneVal) ){
+        setErrorMsg(phone, 'Enter 10 digits number(Digits only');
+        
     }
+    // else if(phoneVal < 10){
+    //     setErrorMsg(phone, 'Enter 10 digits number');
+    // }
     else{
         setSuccessMsg(phone);
     }
-*/
+
    
 
 
--- login.js
+++ login.js
@@ -17,7 +17,7 @@ function getInfo(){
 }
 
 */
-
+// Login ID & passowrd dictonary
 var database = [
     {
         username : "root",
@@ -32,6 +32,8 @@ var database = [
         password : "sanchit1234"
     }
 ]
+
+var attempt = 5; // Variable to count number of attempts
 function validate(){
     var username = document.getElementById("username").value
     var password = document.getElementById("password").value
@@ -40,14 +42,32 @@ function validate(){
             alert("Successfull Login, Hola Visionary");
             //console.log("Successfull Login, Hola Visionary");
             return
-        }//else{
+        }
+        
+            
+            
+
+        
+        //else{
             //alert("Login Failed!! Try with correct username & password");
             //console.log("Login Failed!! Try Again..");
         //}
-        
-    }alert("Login Failed!! Try with correct username & password");
-    //console.log("Login Failed!! Try Again..");
+         
+    }
+    attempt --;// Decrementing by one.
+    alert("You have left "+attempt+" attempt;");
+    // Disabling fields after 3 attempts.
+    if( attempt == 0){
+    document.getElementById("username").disabled = true;
+    document.getElementById("password").disabled = true;
+    document.getElementById("login").disabled = true;
+    return false;
+    }
+    // alert("Login Failed!! Try with correct username & password");
+            //console.log("Login Failed!! Try Again..");
 }
+
+
 /*
 function validate(){
     var username = document.getElementById("username").value;
